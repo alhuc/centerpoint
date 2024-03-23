0b00902a2fe68795e5fe556b46f4a2b8afea0040
Alex Cooper/bin/python3 <ahcoope3@ncsu.edu>
2024-03-23 18:33:03 -0400

    updates to Hellys, reduce to Radon's

--- Hellys.py
+++ Hellys.py
@@ -3,33 +3,31 @@ config.verbosity = "WARNING"
 class Hellys(Scene):
     def construct(self):
         self.camera.background_color = WHITE
-        title = Tex(r'Helly\'s Theorem', color = GREY, font_size = 24)
-        self.play(title.animate.move_to([0,3.5,0]))
+        title = Tex(r'Helly\textquotesingle s Theorem', color = GREY, font_size = 16).move_to([0,3.5,0])
+        self.add(title)
+        title2 = Tex(r'Helly\textquotesingle s Theorem $\rightarrow$ \textbf{Radon\textquotesingle s Lemma}', color = GREY_D, font_size = 16).move_to([0,3.5,0])
+
+        #self.play(title.animate.move_to([0,3.5,0]))
         thm1 = Tex(r'Let $C_1, \ldots, C_n$ be convex sets in $\mathbb{R}^d$, n $\geq$ d+1.', color=BLACK, font_size= 32)
-        self.play(thm1.animate.move_to([0,3, 0]))
+        self.play(thm1.animate.move_to(UP).next_to(title, DOWN))
         self.wait(1)
         self.next_section()
 
-        ## FIXME: should define in terms of points
-        sq = Square(color=BLACK)
-        pent = RegularPolygon(n = 5, color = BLUE).scale(2)
-        poly = RegularPolygon(n = 6, color = ORANGE)
-        tr = Triangle(color = GREEN).scale(1.5)
-        li = [sq, pent, poly, tr]
+        pent = Polygon([-2,0,0], [0,1,0],[2,0,0],[2,-2,0], [-2, -2,0], color=BLUE_A)
+        sq = Polygon([-3,1,0], [-2,-1,0], [0,0,0], [-1,2,0], color=RED_A)
+        tr = Polygon([-2,1,0], [1,1,0], [1, -2.5,0], color=GREEN)
+        hexa = Polygon([0,1.5,0],[-1,0,0],[0,-2,0],[2,-2,0],[2.5,0,0],[2,1.5,0], color=PURPLE)
+
+        li = [pent, sq, tr, hexa]
         self.show_sets(li)
         self.remove(thm1)
-        thm2 = Tex(r'Suppose that the intersection of every d+1 of these sets is nonempty.', color=BLACK, font_size=32).move_to([0,3,0])
+        thm2 = Tex(r'Suppose that the intersection of every d+1 of these sets is nonempty.', color=BLACK, font_size=32).move_to([0,3.2,0])
         self.play(ReplacementTransform(thm1, thm2))
         self.wait(2)
         self.next_section()
 
-
-        sq.move_to([-1.5,0,0])
-        pent.move_to([-.5,-.25,0])
-        poly.move_to([0,0,-.5])
-        tr.move_to([0,.5,.25])
-        itxs, itx_sets, itx_points = self.show_intersections(li)
-        thm3 = Tex(r'Then the intersection of all the $C_i$ is nonempty.', color=BLACK, font_size=32).move_to([0,3,0]) 
+        itxs, itx_sets = self.show_intersections(li)
+        thm3 = Tex(r'Then the intersection of all the $C_i$ is nonempty.', color=BLACK, font_size=32).move_to([0,3.2,0]) 
         self.play(ReplacementTransform(thm2, thm3))
         self.wait(2)
         self.play(thm3.animate.move_to([0,3, 0]).set_color(RED).scale(.75))
@@ -37,12 +35,28 @@ class Hellys(Scene):
         for i in itxs:
             self.remove(i)
         self.wait(2)
-        set_pos = [[-3,3,0], [3,3,0], [-3,-3,0], [3, -3,0]]
+        set_pos = [[-3,2,0], [-3,-2,0], [3,2,0], [3, -2,0]]
+        proof1 = Tex(r'For each $d+1$ set, fix point $a_i$ at it\textquotesingle s intersection.', color=BLACK, font_size=32).move_to([0,3.2,0])
+        self.play(ReplacementTransform(thm3, proof1))
+        a_is = VGroup()
+        targets = [[1.5,1.5,0], [-1.5,1.5,0], [-1.5,-1.5,0], [1.5,-1.5,0]]
         for shape_set, pos in zip(itx_sets, range(len(set_pos))):
-            self.play(Create(shape_set))#*[s.scale(.5) for s in shape_set]))
-            shape_set.shift(set_pos[pos])
-            self.wait(2)
+            self.play(Create(shape_set.scale(.5).shift(set_pos[pos])))
+            a_i = Dot(shape_set.get_center(), color=LOGO_BLACK)  
+            a_is.add(a_i)
+            self.play(Create(a_i))
+            self.wait(.5)
+        for shape_set in itx_sets: 
+            self.remove(shape_set)
+        #self.play(a_is.animate.shift(LEFT))
+        move_ai = [ApplyMethod(a_i.move_to,loc) for a_i,loc in zip(a_is,targets)]
+        proof2 = Tex(r'Collecting $a_i$, Radon\'s allows partition into $I_1, I_2$', color=BLACK, font_size=32).move_to([0,3.2,0]) # TODO pick an x, show I_n
+        self.play(ReplacementTransform(proof1, proof2))
+        self.play(*move_ai)
+
+        self.play(ReplacementTransform(title, title2))
         self.wait(2)
+        
 
     def show_sets(self, li):
         for shape, i in zip(li, range(1,5)):
@@ -56,7 +70,6 @@ class Hellys(Scene):
     def show_intersections(self, li):
         colors = [BLUE, RED, GREEN, ORANGE]
         itxs = []
-        itx_pts = []
         d1_sets = []
         for obj, col in zip(li, colors):
             subli = list(set(li) - set(obj))
@@ -67,7 +80,6 @@ class Hellys(Scene):
                 self.wait(.5)
             inter = Intersection(*[obj for obj in subli], color = col, fill_opacity=0.5 )
             itxs.append(inter)
-            itx_pts.append(inter.get_center)
             self.play(Create(inter))
             set_grp = VGroup()
             for obj1 in subli: 
@@ -75,4 +87,4 @@ class Hellys(Scene):
                 self.remove(obj1)
             d1_sets.append(set_grp)
             self.wait(1)
-        return itxs, d1_sets, itx_pts
+        return itxs, d1_sets
