6dad45bfa8e15ad5ec09060c57d3a02ba98d7f79
jrnolan2 <jrnolan2@ncsu.edu>
2024-03-23 17:21:20 -0400

    Create intro to Radon theorem animation

---
+++ Radon.py
@@ -0,0 +1,36 @@
+from manim import *
+config.verbosity = "WARNING" 
+class Radon(Scene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        title = Tex(r'Radon$\textquotesingle$s Theorem', color = GREY, font_size = 24)
+        self.play(title.animate.move_to([0,3.5,0]))
+        thm1 = Tex(r'Let $A$ be a set of $d+2$ points in $\mathbf{R}^d$.', color = BLACK, font_size = 32)
+        self.play(thm1.animate.move_to([0,3,0]))
+        self.wait(1)
+        
+        dimensions = Tex(r'Dimension: $d$', color = BLACK, font_size = 32)
+        self.play(dimensions.animate)
+        example_dimension = Tex(r'Let$\textquotesingle$s look at 2-D for example', color = BLACK, font_size = 32)
+        example_dimension.move_to([0,1,0])
+        self.play(example_dimension.animate)
+        dimensions_two = Tex(r'Dimension: $2$', color = BLACK, font_size = 32)
+        self.play(ReplacementTransform(dimensions, dimensions_two))
+        points = Tex(r'Points:', color = BLACK, font_size = 32)
+        points.move_to([0,-1,0])
+        self.play(points.animate)
+        d_plus_two_points = Tex(r'$d + 2$', color = BLUE_E, font_size = 32)
+        two_plus_two_points = Tex(r'$2 + 2$', color = BLUE_E, font_size = 32)
+        four_points = Tex(r'$4$', color = BLUE_E, font_size = 32)
+        d_plus_two_points.move_to([1,-1,0])
+        two_plus_two_points.move_to([1,-1,0])
+        four_points.move_to([1,-1,0])
+        self.play(d_plus_two_points.animate)
+        self.play(ReplacementTransform(d_plus_two_points, two_plus_two_points))
+        self.play(ReplacementTransform(two_plus_two_points, four_points))
+        self.remove(thm1)
+        self.remove(example_dimension)
+        self.remove(dimensions_two)
+        self.remove(points)
+        self.remove(four_points)
+        self.wait(1)
