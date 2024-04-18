34c41ef96e35a90caa7de2033426b554edc06cac
alhuc <cooper.alex.h@gmail.com>
2024-04-18 16:54:38 -0400

    Refactoring, Potential show_sets

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# centerpoint
-Spring 2024 CSC591 Computational Geometry Project on the Centerpoint Theorem
+# Centerpoint Theorem
+Spring 2024 CSC591 Computational Geometry Project on the Centerpoint Theorem.
+
+We base our interactive proof on Matoušek's proof in Section 1.4 of *Lectures on Discrete Geometry*.
+
--- centerpoint_mpl.py
+++ centerpoint_mpl.py
@@ -1,24 +1,42 @@
 from matplotlib.backend_bases import MouseButton
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import numpy as np
 from itertools import combinations
 from scipy.spatial import ConvexHull
 import math
-
 def tellme(s):
     print(s)
-    plt.title(s, fontsize=12)
+    plt.suptitle(s, wrap=True)
     plt.draw()
 
 def create_pts(plt):
+    """
+    Description
+    
+    Parameters 
+    ----------
+    plt:
+        Description
+    """
     pt_set = []
-    tellme('Select n of points, right click to remove recent, center click to finish ')
+    tellme('Select $n$ points. Right click to remove last added. Press anything else to finish selection.')
     pt_set = np.asarray(plt.ginput(-1, timeout=-1, mouse_stop = MouseButton.MIDDLE))
     for i in pt_set:
         plt.scatter(x=i[0], y=i[1])
     return pt_set   
 
 def gen_combinations(pt_set, hspace_cond):
+    """
+    Description
+    
+    Parameters 
+    ----------
+    pt_set : 
+        Description
+    hspace_cond:
+        Description
+    """
     filtered_combs = []
     """ignore any |combinations| < hspace_cond"""
     for i in range(math.ceil(hspace_cond), len(pt_set)+1):
@@ -26,49 +44,68 @@ def gen_combinations(pt_set, hspace_cond):
         filtered_combs.extend([list(comb) for comb in combo_r])
     return filtered_combs
 
-def main(): 
-    plt.figure()
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
-    pt_set = create_pts(plt)
-    hspace_cond = 2/3 * len(pt_set)
-    pt_combinations = gen_combinations(pt_set, hspace_cond)
+def find_compact_conv_sets(pt_set, pt_combinations):
+    """
+    Description
+    
+    Parameters 
+    ----------
+    pt_set : 
+        Description
+    pt_combinations:
+        Description
+    """
     valid_combs = []
-    print(pt_combinations)
+    
+    def _get_unique_combinations(combinations): 
+        unique_valid_combs = []
+        for comb in combinations:
+            if not any(np.array_equal(comb, unique) for unique in unique_valid_combs):
+                unique_valid_combs.append(comb)
+        return unique_valid_combs 
+
     for comb in pt_combinations: 
-        print(comb)
-        """normal equations in 2D list:[ x, y, offset] """
+        """normal equations in 2D list:[ [x, y, offset] , ... , ] """
         hull_eq = ConvexHull(comb).equations
-        
         """find family of compact convex sets C that satisfy hspace_cond
            eq (hyperplane) = line in 2D = [ x , y , offset]"""
         for eq in hull_eq: 
             a, b, c = eq
             eq_count = 0
-            """subset pt_set: compare comb elements against pt_set, axis=1 groups by row ((x, y) pair)
-                          ~ takes subset of entries that are false"""
             pts_not_in_comb = pt_set[np.isin(pt_set, comb, invert=True).all(axis=1)] 
             for pt in pts_not_in_comb:
                 x, y = pt 
-                ## line test
                 if (a*x + b*y <= c):
                     eq_count += 1
             if eq_count == 0:
-                ## never entered
-                print("inside append")
                 valid_combs.append(comb)
-    unique_valid_combs = []
-    for comb in valid_combs:
-        if not any(np.array_equal(comb, unique) for unique in unique_valid_combs):
-            unique_valid_combs.append(comb)
-#    unique_valid_combs = np.unique(valid_combs,axis=0)
-    print(unique_valid_combs)
-    plt.show()
-    return valid_combs
 
-#This is for drawing convex closures
-#Example: plt.fill(points[hull.vertices,0], points[hull.vertices,1], 'r', lw=2)
+    compact_conv_sets = _get_unique_combinations(valid_combs) 
+    return compact_conv_sets
+
+
+def show_sets(c_sets, plt):
+    ## viridis is just a standard uniform colormap
+    cmap = plt.cm.get_cmap('viridis', len(c_sets))
+    colors = cmap(np.linspace(0,1, len(c_sets)))
+    new_map = ListedColormap(colors)
+    for color, pt_comb in enumerate(c_sets):
+        # assuming pt_comb are np.arrays
+        plt.fill(pt_comb[:,0], pt_comb[:,1], color = new_map(color), alpha = 0.3)
+
+def main(): 
+    plt.figure()
+    plt.style.use('fast')
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    pt_set = create_pts(plt)
+    hspace_cond = 2/3 * len(pt_set)
+    pt_combinations = gen_combinations(pt_set, hspace_cond)
+    compact_conv_sets = find_compact_conv_sets(pt_set, pt_combinations)
+    print(compact_conv_sets)
+    # show_sets(compact_conv_sets, plt)
+    plt.show()
+
 
 if __name__ == '__main__': 
     main()
-    #print(valid_sets)
